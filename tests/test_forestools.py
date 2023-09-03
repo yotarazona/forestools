9a042e86264ac57d4fb8338adf7ce0f60090bac4
Yonatan Tarazona <ytarazona09@gmail.com>
2023-09-03 12:32:13 -0500

    Create test_forestools.py

---
+++ tests/test_forestools.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Tests for `forestools` package."""
+
+
+import unittest
+
+from forestools import pvts
+
+class TestForestools(unittest.TestCase):
+    """Tests for `forestools` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
